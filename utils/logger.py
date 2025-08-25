d8bd53702d95933b8910c8ed900c30678128b616
Diam177 <92233992+Diam177@users.noreply.github.com>
2025-08-25 14:45:22 +0200

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,18 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+def get_logger(name: str = "app", log_path: str = "app.log") -> logging.Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
+    logger.addHandler(ch)
+    fh = RotatingFileHandler(log_path, maxBytes=1_000_000, backupCount=2, encoding="utf-8")
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s"))
+    logger.addHandler(fh)
+    return logger
 
