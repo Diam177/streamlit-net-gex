9e2243e7168e811207725f2a48540b9a195eda2a
Diam177 <92233992+Diam177@users.noreply.github.com>
2025-08-25 17:01:54 +0200

    Delete services/__init__.py

--- services/__init__.py
+++
@@ -1,5 +0,0 @@
-# services/__init__.py
-# Без сайд-эффектов. Экспортируем только то, что реально есть.
-from .api_client import get_option_chain, ApiError
-
-__all__ = ["get_option_chain", "ApiError"]
